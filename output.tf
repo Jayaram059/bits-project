148e51751f43f3d17ff31e48a952f106e9a7b196
Jayaram Prasad <jayaram059@gmail.com>
2024-09-07 22:46:35 +0530

    Cleanup

--- output.tf
+++
@@ -1,7 +0,0 @@
-output "dynamic_group_ocid" {
-  value = module.bitsproject_dynamic_group.dynamic_group_ocid
-}
-
-output "policy_ocid" {
-  value = module.bitsproject_policies.policy_ocid
-}
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,8 @@
-# Output the "list" of all availability domains.
-output "all-availability-domains-in-your-tenancy" {
-  value = data.oci_identity_availability_domains.ads.availability_domains
+output "dynamic_group_ocid" {
+  value = module.bitsproject_dynamic_group.dynamic_group_ocid
 }
+
+output "policy_ocid" {
+  value = module.bitsproject_policies.policy_ocid
+}
+
