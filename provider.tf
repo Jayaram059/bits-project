787b290f3451923ce4f13eabce4b684ac20098c1
tadimeti jayaram prasad <66716567+Jayaram059@users.noreply.github.com>
2024-09-07 11:49:06 +0530

    TF State to Object Storage Worked

--- provider.tf
+++ provider.tf
@@ -5,3 +5,21 @@ provider "oci" {
   fingerprint = "cb:86:38:8f:40:f0:1d:ab:9c:ae:0e:34:30:8b:ec:33"
   region = "ap-hyderabad-1"
 }
+
+# (Terraform version >= 1.6.4)
+terraform {
+  backend "s3" {
+    bucket                    = "terraform-states"
+    region                    = "ap-hyderabad-1"
+    key                       = "tf.tfstate"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
+    skip_s3_checksum            = true
+    skip_metadata_api_check = true
+    endpoints = {
+      s3 = "https://axvjenufkdre.compat.objectstorage.ap-hyderabad-1.oraclecloud.com"
+    }
+  }
+}
